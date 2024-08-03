53eeac6667f3e76ba7bb23270b6af4175ceca1f0
melekmoalla <melekmoalla2468@gmail.com>
2024-08-03 18:49:37 +0100

    test

--- markdown2html.py
+++ markdown2html.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""
-markdown2html.py: A script that converts a Markdown file to an HTML file.
 
-Usage:
-    ./markdown2html.py README.md README.html
-"""
 import sys
 import os
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
+    if len(sys.argv) != 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
     if not os.path.isfile(sys.argv[1]):
