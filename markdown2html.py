03921ed769c14f36202d22d9031050a38f7a7d16
melekmoalla <melekmoalla2468@gmail.com>
2024-08-03 17:52:32 +0100

    correct code

--- markdown2html.py
+++ markdown2html.py
@@ -8,7 +8,7 @@ import os
 
 def main():
     if (sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html")
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
     if not os.path.isfile(sys.argv[1]):
         print(f'Missing {sys.argv[1]}', file=sys.stderr)
