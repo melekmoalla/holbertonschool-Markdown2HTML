1c6d3213f51088f1be78acc56d9195721e56ebb6
melekmoalla <melekmoalla2468@gmail.com>
2024-08-03 18:29:58 +0100

    t

--- markdown2html.py
+++ markdown2html.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+import sys
+import os
+
 """
 markdown2html.py: A script that converts a Markdown file to an HTML file.
 
 Usage:
     ./markdown2html.py README.md README.html
 """
-import sys
-import os
 
 
 def main():
