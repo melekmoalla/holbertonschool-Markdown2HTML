8eabef30071241494de927813a59bfef604ac7e2
melekmoalla <melekmoalla2468@gmail.com>
2024-08-03 17:54:14 +0100

    test

--- markdown2html.py
+++ markdown2html.py
@@ -9,12 +9,11 @@ import os
 def main():
     if (sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-        sys.exit(1)
+        exit(1)
     if not os.path.isfile(sys.argv[1]):
         print(f'Missing {sys.argv[1]}', file=sys.stderr)
-        sys.exit(1)
-    else:
-        sys.exit(0)
+        exit(1)
+    exit(0)
 
 
 if __name__ == "__main__":
