83fad786ea075dd4788cd3b0b39867da2c7783e7
melekmoalla <melekmoalla2468@gmail.com>
2024-08-03 19:15:48 +0100

    test

--- markdown2html.py
+++ markdown2html.py
@@ -17,7 +17,7 @@ def main():
     elif not os.path.isfile(sys.argv[1]):
         print(f'Missing {sys.argv[1]}', file=sys.stderr)
         exit(1)
-
-
+    print()
+    exit(0)
 if __name__ == "__main__":
     main()
