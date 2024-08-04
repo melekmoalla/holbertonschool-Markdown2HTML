9d4ccdf4c42a3c89c437546461c156d28c1a9b2c
melekmoalla <melekmoalla2468@gmail.com>
2024-08-04 14:26:54 +0100

    test

--- markdown2html.py
+++ markdown2html.py
@@ -22,17 +22,38 @@ def main():
 
     with open(sys.argv[1], 'r') as f:
         content = f.readlines()
-
+    ##
+    line_html=[]
+    m = 0
+    for line in content:
+        if line[0] == '-' :
+            if m == 0:
+                m = 1
+                line_html.append('<ul>')
+            line_html.append(f"<li>{line[2:].strip()}</li>")
+        else:
+            if m == 1:
+                m = 0
+                line_html.append("</ul>")
+            line_html.append(line)
+    if m == 1:
+        line_html.append("</ul>")
+        
+    ##
     html_lines = []
-    for i in content:
-        b = 0
-        for a in i:
-            if a == '#':
-                b += 1
-        if b > 0 and b <= 6:
-            text = f"<h{b}>{i[b:].strip()}</h{b}>"
-            # print(text)
-            html_lines.append(text)
+    print(line_html)
+    for i in line_html:
+        if i[0] == '#':
+            b = 0
+            for a in i:
+                if a == '#':
+                    b += 1
+            if b > 0 and b <= 6:
+                text = f"<h{b}>{i[b:].strip()}</h{b}>"
+                # print(text)
+                html_lines.append(text)
+        else:
+            html_lines.append(i)
 
     with open(sys.argv[2], 'w') as f:
         f.write("\n".join(html_lines))
