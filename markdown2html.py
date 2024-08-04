810f560e2620e730e90ddc0f932527433e227186
melekmoalla <melekmoalla2468@gmail.com>
2024-08-04 13:16:58 +0100

    pep8

--- markdown2html.py
+++ markdown2html.py
@@ -25,9 +25,9 @@ if __name__ == '__main__':
         b = 0
         for a in i:
             if a == '#':
-                b +=1
+                b += 1
         text = f"<h{b}>{i[b:].strip()}</h{b}>"
-        #print(text)
+        # print(text)
         html_lines.append(text)
     with open(sys.argv[2], 'w') as f:
         f.write("\n".join(html_lines))
