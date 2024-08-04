8ca4ae00d8e3522bc7dbaa28a777c10204595521
melekmoalla <melekmoalla2468@gmail.com>
2024-08-04 13:19:45 +0100

    corret

--- markdown2html.py
+++ markdown2html.py
@@ -26,9 +26,11 @@ if __name__ == '__main__':
         for a in i:
             if a == '#':
                 b += 1
-        text = f"<h{b}>{i[b:].strip()}</h{b}>"
-        # print(text)
-        html_lines.append(text)
+        if b > 0 and b <= 6:
+            text = f"<h{b}>{i[b:].strip()}</h{b}>"
+            # print(text)
+            html_lines.append(text)
     with open(sys.argv[2], 'w') as f:
         f.write("\n".join(html_lines))
+
     sys.exit(0)
